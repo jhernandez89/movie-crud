b391fc21fa14d478ee3b3682d66eb8980b719ca0
Jeff Miguel Hernandez <jeffhernandez89@gmail.com>
2017-02-27 10:11:04 -0700

    new homepage

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const routes = require('./routes.js');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
-app.use(express.static('./public/'));
+app.use(express.static('./public/', { 'index': 'home.html' }));
 app.use(bodyParser.json());
 app.use('/', routes);
 
