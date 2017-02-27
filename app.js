e27e5b4ccbdca6c31966966efda3ec6136a15cf4
Jeff Miguel Hernandez <jeffhernandez89@gmail.com>
2017-02-27 10:09:03 -0700

    port dynamically created

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const express = require('express');
 const app = express();
 const routes = require('./routes.js');
 const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
 
 app.use(express.static('./public/'));
 app.use(bodyParser.json());
 app.use('/', routes);
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.listen(port, () => {
+  console.log('Server listening on port ', port);
 });
